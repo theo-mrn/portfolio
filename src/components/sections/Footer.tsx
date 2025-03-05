7d476c6e239adbf048cad247fbccdc94597befd3
Theo Morin <theo.morin2@edu.devinci.fr>
2025-03-05 18:37:20 +0100

    footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,10 +1,13 @@
+import { config } from "@/app/config";
+
+
 export function Footer() {
   return (
     <footer className="py-8 border-t">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="flex items-center gap-2">
-            <h3 className="font-bold">Theo Morin</h3>
+            <h3 className="font-bold">{config.name}</h3>
             <span className="text-sm text-muted-foreground">© {new Date().getFullYear()}</span>
           </div>
           <p className="text-sm text-muted-foreground text-center md:text-right">
