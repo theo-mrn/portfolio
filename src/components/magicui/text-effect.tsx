d25a5b465cc13ec46fb1952779118cbb88000459
theo-mrn <theo.morin1402@gmail.com>
2025-04-27 00:23:52 +0200

    V2

--- src/components/magicui/text-effect.tsx
+++ src/components/magicui/text-effect.tsx
@@ -178,7 +178,7 @@ const createVariantsWithTransition = (
 ): Variants => {
   if (!transition) return baseVariants;
 
-  const { exit: _, ...mainTransition } = transition;
+  const {  ...mainTransition } = transition;
 
   return {
     ...baseVariants,
